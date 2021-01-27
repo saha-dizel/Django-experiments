c831c793b69d752c86881a1e635cb15d7004719b
Aliaksandr Kharavets <saha.dizel@gmail.com>
2021-01-28 01:58:27 +0300

    Add login and very old-school logout system

--- djangoFirst/tutorialBlogApp/urls.py
+++ djangoFirst/tutorialBlogApp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 from userManagerApp import views as register_views
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
-    path('register/', register_views.register_view, name='blog-register')
+    path('register/', register_views.register_view, name='blog-register'),
+    path('login/', auth_views.LoginView.as_view(template_name='userManagerApp/login.html'), name='blog-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='userManagerApp/logout.html'), name='blog-logout'),
+    path('profile/', register_views.user_profile, name='blog-profile')
 ]
--- djangoFirst/userManagerApp/views.py
+++ djangoFirst/userManagerApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -10,8 +11,12 @@ def register_view(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account successfully created for {username}')
-            return redirect('blog-home')
+            messages.success(request, f'Account successfully created for {username}! You can now log in! Yay!')
+            return redirect('blog-login')
     else:
         form = UserRegistrationForm()
     return render(request, 'userManagerApp/register.html', {'form': form})
+
+@login_required
+def user_profile(request):
+    return render(request, 'userManagerApp/profile.html')
