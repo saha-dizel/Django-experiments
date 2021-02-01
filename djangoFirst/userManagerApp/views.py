07b9aa99cbf99437b4c1f55728fd24acbb128804
Aliaksandr Kharavets <saha.dizel@gmail.com>
2021-02-02 01:28:57 +0300

    Add profile modification, image resizing and small update to home page

--- djangoFirst/userManagerApp/views.py
+++ djangoFirst/userManagerApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -17,6 +17,24 @@ def register_view(request):
         form = UserRegistrationForm()
     return render(request, 'userManagerApp/register.html', {'form': form})
 
+
 @login_required
 def user_profile(request):
-    return render(request, 'userManagerApp/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Account updated. Hopefully.')
+            return redirect('blog-profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'userManagerApp/profile.html', context)
